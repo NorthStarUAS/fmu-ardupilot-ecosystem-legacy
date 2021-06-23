2679d76215ed0382302b6c4a9e5b16c27eb510d1
Curtis L. Olson <curtolson@flightgear.org>
2021-06-23 11:04:34 -0500

    The larger plan is to migrate to a json based config file system stored on an SD card.  The old message-based system is now commented out.  I'll depricate this is stages because I want to make it easy to find the code to read/write from eeprom which was painstakenly worked out.  I may want to save a copy of the json config in eeprom (if the config is smaller, or the eeprom is larger) or store other things there.

--- src/config.h
+++ src/config.h
@@ -13,17 +13,17 @@ private:
     
     AP_HAL::Storage *storage = NULL;
     
-    int config_size = 0;
-    struct packed_config_t {
-        message::config_airdata_t::_compact_t airdata;
-        message::config_board_t::_compact_t board;
-        message::config_ekf_t::_compact_t ekf;
-        message::config_imu_t::_compact_t imu;
-        message::config_mixer_matrix_t::_compact_t mixer_matrix;
-        message::config_power_t::_compact_t power;
-        message::config_pwm_t::_compact_t pwm;
-        message::config_stability_damping_t::_compact_t stab;
-    };
+    // int config_size = 0;
+    // struct packed_config_t {
+    //     message::config_airdata_t::_compact_t airdata;
+    //     message::config_board_t::_compact_t board;
+    //     message::config_ekf_t::_compact_t ekf;
+    //     message::config_imu_t::_compact_t imu;
+    //     message::config_mixer_matrix_t::_compact_t mixer_matrix;
+    //     message::config_power_t::_compact_t power;
+    //     message::config_pwm_t::_compact_t pwm;
+    //     message::config_stability_damping_t::_compact_t stab;
+    // };
 
     PropertyNode config_node;
     
@@ -31,14 +31,14 @@ private:
     
     config_t();
     
-    message::config_airdata_t airdata_cfg;
-    message::config_board_t board_cfg;
-    message::config_ekf_t ekf_cfg;
-    message::config_imu_t imu_cfg;
-    message::config_mixer_matrix_t mixer_matrix_cfg;
-    message::config_power_t power_cfg;
-    message::config_pwm_t pwm_cfg;
-    message::config_stability_damping_t stab_cfg;
+    // message::config_airdata_t airdata_cfg;
+    // message::config_board_t board_cfg;
+    // message::config_ekf_t ekf_cfg;
+    // message::config_imu_t imu_cfg;
+    // message::config_mixer_matrix_t mixer_matrix_cfg;
+    // message::config_power_t power_cfg;
+    // message::config_pwm_t pwm_cfg;
+    // message::config_stability_damping_t stab_cfg;
 
     uint16_t serial_number = 0;
     uint16_t read_serial_number();
@@ -47,8 +47,8 @@ private:
     int write_storage();
 
     void actuator_gain_defaults();
-    void force_config_aura3();
-    void force_config_goldy3();
+    //void force_config_aura3();
+    //void force_config_goldy3();
     void reset_defaults();
 
     void setup();               // load config.json from sd card
