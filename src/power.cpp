99de55fd9d8f68b42908b36fe75ef6ac6929881d
Curtis L. Olson <curtolson@flightgear.org>
2021-08-02 14:00:01 -0500

    Configurable main battery voltage divider.

--- src/power.cpp
+++ src/power.cpp
@@ -8,12 +8,17 @@
 static float amps_offset = 0.0;
 
 void power_t::init() {
+    PropertyNode config_node("/config/power");
     power_node = PropertyNode("/sensors/power");
     PropertyNode("/sensors/power");
     hal.scheduler->delay(1000);
+    batt_volt_divider = AP_BATT_VOLTDIVIDER_DEFAULT;
+    if ( config_node.hasChild("batt_volt_divider") ) {
+        batt_volt_divider = config_node.getDouble("batt_volt_divider");
+    }
     console->printf("Battery volt pin: %d\n", AP_BATT_VOLT_PIN);
     console->printf("Battery current pin: %d\n", AP_BATT_CURR_PIN);
-    console->printf("Volt divider: %.2f\n", AP_BATT_VOLTDIVIDER_DEFAULT);
+    console->printf("Volt divider: %.2f\n", batt_volt_divider);
     console->printf("Amp per volt: %.2f\n", AP_BATT_CURR_AMP_PERVOLT_DEFAULT);
     _volt_pin_analog_source = hal.analogin->channel(AP_BATT_VOLT_PIN);
     _curr_pin_analog_source = hal.analogin->channel(AP_BATT_CURR_PIN);
@@ -26,7 +31,7 @@ void power_t::update() {
     
     // battery volts / amps
     battery_volts = _volt_pin_analog_source->voltage_average()
-        * AP_BATT_VOLTDIVIDER_DEFAULT;
+        * batt_volt_divider;
     battery_amps = (_curr_pin_analog_source->voltage_average() - amps_offset)
         * AP_BATT_CURR_AMP_PERVOLT_DEFAULT;
     power_node.setDouble("battery_volts", battery_volts);
--- src/power.h
+++ src/power.h
@@ -10,6 +10,7 @@ private:
     PropertyNode power_node;
     AP_HAL::AnalogSource *_volt_pin_analog_source;
     AP_HAL::AnalogSource *_curr_pin_analog_source;
+    float batt_volt_divider = 0.0;
     
 public:
     float avionics_v = 0.0;
