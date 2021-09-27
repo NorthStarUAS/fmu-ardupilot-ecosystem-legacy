1d2a112d77c0444716e55a106cb25c9d08ba7028
Curtis L. Olson <curtolson@flightgear.org>
2021-09-27 09:13:16 -0500

    Use gps as input to ground altitude filter. Don't try to create a filter based agl metric for now. Maybe we can add it later, or if a module wants to use this it can derive the value itself from current filter altitude - ground altitude.

--- src/state/ground.cpp
+++ src/state/ground.cpp
@@ -4,32 +4,31 @@
 // initialize ground estimator variables
 void ground_est_t::init() {
     airdata_node = PropertyNode( "/sensors/airdata" );
-    nav_node = PropertyNode( "/filters/nav" );
+    gps_node = PropertyNode( "/sensors/gps/0" );
     pos_node = PropertyNode( "/position" );
     ground_alt_filt.set_time_factor(30.0);
     ground_alt_calibrated = false;
 }
 
 void ground_est_t::update(float dt) {
-    if ( nav_node.getInt("status") == 2 ) {
-        // determine ground reference altitude.  Average nav altitude
-        // over the most recent 30 seconds that we are !is_airborne
-        if ( !ground_alt_calibrated and nav_node.getInt("status") == 2 ) {
+    // determine ground reference altitude.  Average gps altitude over
+    // the most recent 30 seconds that we are !is_airborne
+    if ( gps_node.getInt("status") >= 3 ) {
+        if ( !ground_alt_calibrated ) {
             ground_alt_calibrated = true;
-            ground_alt_filt.init( nav_node.getDouble("altitude_m") );
+            // subtract out airdata agl in case we somehow we are initing
+            // in air (i.e. took off before gps had a good fix?)
+            double ground_m = gps_node.getDouble("altitude_m")
+                - airdata_node.getDouble("altitude_agl_m");
+            ground_alt_filt.init( ground_m );
         }
 
         if ( !airdata_node.getBool("is_airborne") ) {
             // update ground altitude estimate while not airborne
             // fixme: create a settled (not moving) parameter and use that?
-            ground_alt_filt.update( nav_node.getDouble("altitude_m"), dt );
+            ground_alt_filt.update( gps_node.getDouble("altitude_m"), dt );
             pos_node.setDouble( "altitude_ground_m",
                                 ground_alt_filt.get_value() );
         }
-
-        // fixme: should this move to airdata helper or stay here?
-        float agl_m = nav_node.getDouble( "altitude_m" )
-            - ground_alt_filt.get_value();
-        pos_node.setDouble( "altitude_agl_m", agl_m );
     }
 }
