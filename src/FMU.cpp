a8b34f3411136fa1f6909abc57277837d9af868b
Curtis L. Olson <curtolson@flightgear.org>
2021-11-15 15:04:40 -0600

    Initial revision.

--- src/FMU.cpp
+++ src/FMU.cpp
@@ -154,6 +154,9 @@ void setup() {
     menu.init();
 
     gimbal.init();
+
+    gcs().init();
+    // gcs().setup_console();
     
     printf("Setup finished.\n");
     printf("Ready and transmitting...\n");
@@ -242,6 +245,9 @@ void loop() {
 
         pilot.write();
 
+	gcs().update_receive();
+	gcs().update_send();
+	
         gimbal.update();
         
         // blink the led
@@ -251,8 +257,8 @@ void loop() {
         // status
         status_node.setUInt("available_memory", hal.util->available_memory());
 
-        gcs_link.read_commands();
-        gcs_link.update();
+        //gimbal_link.read_commands();
+        //gimbal_link.update();
         
     
         counter++;
